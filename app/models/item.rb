b77be8c48a2648b700ebaac7adbc2ab34d2ce626
landroide13 <lazaro02@hotmail.com>
2018-09-09 19:28:24 +1200

    Added Image to Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   validates_presence_of :name, :description, :price, :section
   belongs_to :section
+  include Placeholder
+
+  after_initialize :set_default
+
+  def set_default
+    self.image ||= Placeholder.image(height: '350', width: '150')
+  end
+
+
 end
